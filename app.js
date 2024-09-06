1581b7b39c4a205405e6ef7f15a8ec30caf1f858
Muaaz Ahmed Baig <muaazahmed0111@gmail.com>
2024-09-06 13:08:14 +0500

    Assignment no 1 JS

---
+++ app.js
@@ -0,0 +1,51 @@
+// Variables 
+
+var num1 = 20 
+var num2 = 50
+var num3 = 90
+
+console.log(num1 + num2 + num3);
+
+var fullname = "Muaaz Ahmed Baig"
+// ye string hai ""
+
+var city = "Karachi"
+
+var education = "Bachelors" 
+
+var institute = "Jawan Pakistan"
+
+console.log("hello my name is " + fullname + ". I live in " + city + ". My education is " + education + ". My institute name is " + institute + ".");
+
+var res = num1 + num3
+console.log( typeof res)
+
+// variable decalration 
+var age;
+// undefined variable 
+age = 22
+console.log(age);
+
+// variable decalration and initialization 
+var age = 22
+// blue se ayga console me to number hai or white or black se ayga to string hai 
+console.log(age);
+
+var name = "muaaz ahmed"
+
+name = "muaaz ahmed baig"
+
+console.log(name);
+
+
+// Camel case 
+var FirstName = "muaaz"
+// snake case 
+var first_name = "muaaz"
+
+// data types 
+
+// alert("sorry it's not working")
+
+// Number;
+// String;
---
+++ java.js
@@ -0,0 +1,166 @@
+// chapter 1 (alert)
+// Question no 1  
+
+// alert("Hello!"); 
+
+// Question no 2 
+
+// alert("Error: please enter a valid password");
+
+// Question no 3 
+
+// alert("Welcome to JS Land... Happy Coding!") 
+
+// Question no 4 
+
+// alert("Welcome to Js land..")
+// alert("Happy Coding!")
+
+// Question no 5 
+
+// console.log("Hello.. I can run JS through my web browser's console")
+
+// Question no 7
+
+// a.yes 
+// b.yes 
+// c.yes 
+// d.yes 
+
+// Chapter no 2 (variables for string)
+
+// Question no 1
+
+// var username;
+
+// Question no 2 
+
+// var myName = "Muaaz Ahmed Baig"
+
+// Question no 3
+
+// a. var message;
+// b. var message = "hello world"
+// alert("hello World")
+
+// Question no 4
+// alert("Jhone Doe")
+// alert("15 years old")
+// alert("Ceertified mobile application development")
+
+// Question no 5
+// alert("pizza\npizz\npiz\npi\np")
+
+// Question no 6
+// var email = "muaazahmed0111@gamil.com"
+// alert(email)
+
+// Question no 7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn the book " + book)
+
+// Question no 8 
+// document.write("Yeh! i can write HTML content through Javascript ")
+
+// Question no 9 
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”")
+// alert("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Chapter No 3 (Variables for numbers )
+
+// Q1 
+// var age = "I am 22 years old"
+// alert(age)
+
+// Q2 
+// var web = "You have visited this site 100 times"
+// alert(web)
+
+// Q3 
+// var birth =  "my birth year is 2002"
+// document.write(birth)
+// document.write(typeof birth)
+
+// Q4 
+// var V_name = "John Doe"
+// var P_title = "5 T-shirt(s)"
+// var cloth = "XYZ Clothing store."
+// document.write(V_name + "ordered " + P_title + "on " + cloth)
+
+// Chater No 4 (variables names legal and illegal )
+
+// Q1 
+// var name = "muaaz ahmed", city = "karachi", age = 22
+
+// Q2
+// legal 
+// var fullname
+// var $name
+// var name1
+// var last_name 
+// var FullName
+
+// illegal 
+// var 12name 
+// var #name
+// var name#
+// var !myname
+// var last-name 
+
+// Word File Chapter No 1 
+
+// Q1 
+// alert("Muaaz Ahmed")
+// alert("Baig")
+// alert("muaazahmed0111@gmail.com")
+// alert("03132332015")
+// alert("muaaz#987")
+
+// Q2 
+// alert("You're learning JavaScript!")
+
+// Q3 
+// alert("Hello, my name is muaaz ahmed and I am Bachelors in HAMDARD UNIVERSITY")
+
+// Chapter 2 (Variables for string)
+
+// Q1 
+// var CamelCase;
+
+// Q2 
+// var first_name;
+// first_name = "Hello World"
+
+// Q3 
+// var TeamName = "my team members is muaaz, fazal and hamza"
+// alert(TeamName)
+
+// Q4 
+// var bestMan = "Charlie"
+// bestMan = "new Charlie"
+
+// Chapter 3 (Variables for numbers) 
+
+// Q1 
+// var caseQty
+
+// Q2 
+// caseQty = 144 
+
+// Q3 
+// var num = 9
+
+// Q4 
+// var my_num
+// my_num = 2+20
+
+// Q5 
+// var merchTotal = 100;
+// var ShippingCharge = 10;
+// var orderTotal = merchTotal + ShippingCharge;
+// console.log(orderTotal)
+
+// Q6 
+// var number = 22
+// totalnumber = number + 28
+// console.log(totalnumber)
